cb00b625d1dd5a9888da597f2dbb7dbe00250eb4
Chirag Agrawal <chiragagrawal844@gmail.com>
2025-08-14 15:59:39 +0530

    Data Types Commit

--- basic.py
+++ basic.py
@@ -65,10 +65,42 @@ print("S"*3)   # This will print "SSS" since it repeats the string 3 times
 
 # Demonstrating string indexing and slicing
 s = "Chirag Agrawal"
-print(s[0])    # This will print 'C', the first character of the string
-print(s[1])    # This will print 'h', the second character of the string
-print(s[6])    # This will print ' ', the seventh character of the string
-print(s[-1])   # This will print 'l', the last character of the string
-print(s[0:5])  # This will print 'Chira', the first five characters of the string
-print(s[6:])   # This will print 'Agrawal', from the seventh character to the end
+print(s[0])         # This will print 'C', the first character of the string
+print(s[1])         # This will print 'h', the second character of the string
+print(s[6])         # This will print ' ', the seventh character of the string
+print(s[-1])        # This will print 'l', the last character of the string
+print(s[0:5])       # This will print 'Chira', the first five characters of the string
+print(s[7:])        # This will print 'Agrawal', from the eigth character to the end
+s[0] = "D"          # This will raise an error since strings are immutable in Python
+s.replace("C", "D") # This will replace 'C' with 'D' in the string, but does not modify the original string
 
+# Demonstrating complex numbers
+x=3+5j
+print(x)  # This will print the complex number (3+5j)
+
+# Demonstrating list operations
+list_1 = [1, 2, 3, 4, "Chirag", [1, 2, 3, 4]]  # A simple list of integers
+print(list_1)      # This will print the entire list
+print(list_1[0])   # This will print the first element of the list, which is 1
+print(list_1[4])   # This will print the fifth element of the list, which is "Chirag"
+print(list_1[5])   # This will print the sixth element of the list, which is another list [1, 2, 3, 4]
+print(list_1[-3])  # This will print the third last element of the list, which is 4
+
+# Demonstrating tuple operations
+tuple_1 = (1, 2, 3, 4, "Chirag", [1, 2, 3, 4])  # A simple tuple of integers
+print(tuple_1)      # This will print the entire tuple
+print(tuple_1[0])   # This will print the first element of the tuple, which is 1
+print(tuple_1[4])   # This will print the fifth element of the tuple, which is "Chirag"
+print(tuple_1[5])   # This will print the sixth element of the tuple, which is another list [1, 2, 3, 4]
+type(tuple_1)       # This will print <class 'tuple'> since tuple_1 is a tuple
+tuple_2=(7)         # This is not a tuple, it's just an integer
+type(tuple_2)       # This will print <class 'int'> since tuple_2 is not a tuple but an integer
+tuple_3=(7,)        # This is a tuple with one element
+
+# Demonstrating set operations
+set_1 = {1, 2, 3, 4, "Chirag", (1, 2, 3, 4),[1, 2, 3, 4]}  # A simple set of integers and a tuple and a list
+# Note: Sets do not allow mutable types like lists, so the list will be ignored
+print(set_1)       # This will print the entire set
+print(1 in set_1)  # This will print True since 1 is in the set
+print(5 in set_1)  # This will print False since 5 is not in the set
+print(set_1[0])    # This will raise an error since sets do not support indexing
