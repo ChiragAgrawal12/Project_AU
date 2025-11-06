574011d2a98049e112a2531d6f1b9ca1d595fdbc
Chirag Agrawal <chiragagrawal844@gmail.com>
2025-11-06 17:09:18 +0530

    Regex pattern

---
+++ regex.py
@@ -0,0 +1,62 @@
+strings23='''  
+"Email: john.doe@example.com",
+    "Contact: +91-9876543210",
+    "TransactionID_12345_DONE",
+    "Username: user_2025!",
+    "Invoice #INV-00458",
+    "Order placed on 2025-11-04",
+    "Visit our site: https://www.learnregex.com",
+    "Temp reading: 37.6°C",
+    "Address: 42B Baker Street, London",
+    "Note—check at 10:30am tomorrow!",
+    "HELLO_world_Regex123",
+    "E-mail: support@company.co.in",
+    "Total Amount = $1499.75",
+    "CustomerID: CUST_0098",
+    "Alert! Invalid password entered 3 times",
+    "Task Completed? Yes/No",
+    "FlightNo: AI-202",
+    "Marks: 78, 82, 91, 65",
+    "Ref: #ABCDEF12345",
+    "ZIP Code: 560034",
+    "File saved as report_v2.1.pdf",
+    "Email-Me@Now.org",
+    "St",
+    "Sot",
+    "Soot",
+    "st",
+    "PASSWORD: My$ecret@123",
+    "Meeting scheduled on Monday, 9th Dec",
+    "Tag: <regex> is powerful </regex>",
+    "User said: 'I love learning REGEX!'",
+    "item-1, item_2, ITEM#3",
+    "Room Temp: -5°C",
+    "IP Address: 192.168.0.1",
+    "Mask",
+    "Lask",
+    "aask",
+    "5GB",
+    "1TB",
+    "11GB",
+    " 123 ","Tasmd",
+    "[pdf]",
+    "[video]","Tog","kog","mog",
+    "UUID: 550e8400-e29b-41d4-a716-446655440000"'''
+import re  
+pattern=r'So*t'
+pattern2=r'[sS]o*[tT]'
+pattern3=r"[A-Za-z]ask"
+pattern4=r"\s\d{3}\s"                                    # \b is for word boundary
+pattern5=r"[A-Z]\w{1,}"
+pattern6=r"...."
+pattern7=r"\[\w+\]"                                      #  "[[]\w+]"         \[ and \] are used to match the literal square brackets in the string.
+pattern8=r"[^Tk]og"
+pattern9=r"^Ta\w+"
+pattern10=r"(\d+)(GB|TB)"
+matches=re.findall(pattern10,strings23)                  # findall returns a list of all matches in the string
+print(matches)                                           # finditer returns an iterator yielding match objects
+
+for ele in strings23:
+    match=re.findall(pattern9,ele)
+    if match:
+        print(ele)
